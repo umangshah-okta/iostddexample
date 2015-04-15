1ff3c0354bed9fc78c2d91b9a30f190b308b115a
Umang Shah <umang.shah@okta.com>
2015-04-14 22:42:34 -0700

    4

--- StackTDD/StackTDD/DMStackCollection.m
+++ StackTDD/StackTDD/DMStackCollection.m
@@ -8,8 +8,26 @@
 
 #import "DMStackCollection.h"
 
+
+@interface DMStackCollection ()
+@property (nonatomic, strong) NSMutableArray *list;
+@end
+
 @implementation DMStackCollection
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.list = [NSMutableArray array];
+    }
+    return self;
+}
+
+- (NSUInteger)count {
+    return self.list.count;
+}
+
 - (void)push:(id)object {
-    
+    [self.list addObject:object];
 }
 @end
--- StackTDD/StackTDDTests/StackTDDTests.m
+++ StackTDD/StackTDDTests/StackTDDTests.m
@@ -11,32 +11,27 @@
 #import "DMStackCollection.h"
 
 @interface StackTDDTests : XCTestCase
-
+@property (nonatomic, strong) DMStackCollection *stack;
 @end
 
 @implementation StackTDDTests
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+     self.stack = [[DMStackCollection alloc] init];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 
-
 - (void) testThatICanCreateACollectionThatOperatesAsAStack {
-    DMStackCollection *stack = [[DMStackCollection alloc] init];
-    NSAssert(stack != nil, @"stack could not be created");
+    NSAssert(self.stack != nil, @"stack could not be created");
 }
 
 - (void) testThatICanPushAnObjectOntoTheStack {
-    DMStackCollection *stack = [[DMStackCollection alloc] init];
-    [stack push: @"something for the stack"];
-    
-    NSAssert(stack.count == 1, @"stack should contain 1 object");
+    [self.stack push: @"something for the stack"];
+    NSAssert(self.stack.count == 1, @"stack should contain 1 object");
 }
 @end
